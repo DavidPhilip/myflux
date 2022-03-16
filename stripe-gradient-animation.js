bd2b9c1ddbf206a79aef8242f94d68eaac7dc187
David Philip Gösenbauer <david.goesenbauer@gmail.com>
2022-03-16 15:20:55 +0100

    fix: animation not starting 1 out of 3 times

--- stripe-gradient-animation.js
+++ stripe-gradient-animation.js
@@ -454,7 +454,7 @@ class Gradient {
       this.material = this.initMaterial(), this.geometry = new this.minigl.PlaneGeometry, this.mesh = new this.minigl.Mesh(this.geometry, this.material)
   }
   shouldSkipFrame(e) {
-      return !!window.document.hidden || (!this.conf.playing || (parseInt(e, 10) % 2 == 0 || void 0))
+      return !!window.document.hidden || !this.conf.playing || void 0
   }
   updateFrequency(e) {
       this.freqX += e, this.freqY += e
